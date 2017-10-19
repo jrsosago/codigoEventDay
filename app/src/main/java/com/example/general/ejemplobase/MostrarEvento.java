453dcf50f4c469da63661408ed938fa34c7519ed
jrsosago <jrsosago@poligran.edu.co>
2017-10-19 11:45:29 -0500

    Se finaliza la parte Front-End de Perfil

--- app/src/main/java/com/example/general/ejemplobase/MostrarEvento.java
+++ app/src/main/java/com/example/general/ejemplobase/MostrarEvento.java
@@ -1,7 +1,10 @@
 package com.example.general.ejemplobase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by sosa on 19/10/2017.
@@ -9,9 +12,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MostrarEvento extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle saveInstanceState) {
-        super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_mostrar_evento);
-    }
+  //  Button perfilMostrarEvento;
+
+    //@Override
+    //protected void onCreate(Bundle saveInstanceState) {
+      //  super.onCreate(saveInstanceState);
+        //setContentView(R.layout.activity_mostrar_evento);
+
+//        perfilMostrarEvento=(Button) findViewById(R.id.imageButtonPerfilMostrarEvento);
+
+//        perfilMostrarEvento.setOnClickListener(new View.OnClickListener() {
+  //          @Override
+    //        public void onClick(View view) {
+      //          Intent i=new Intent(MostrarEvento.this, perfil.class);
+        //        startActivity(i);
+          //      finish();
+            //}
+        //});
+    //}
 }
--- app/src/main/java/com/example/general/ejemplobase/perfil.java
+++ app/src/main/java/com/example/general/ejemplobase/perfil.java
@@ -1,10 +1,34 @@
 package com.example.general.ejemplobase;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by sosa on 19/10/2017.
  */
 
 public class perfil extends AppCompatActivity {
+
+    Button cerrarSesion;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+        cerrarSesion=(Button)findViewById(R.id.buttonCerrarSesion);
+
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(perfil.this, baseEjemplo.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
 }
