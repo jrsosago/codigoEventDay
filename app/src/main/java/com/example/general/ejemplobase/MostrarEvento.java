1349a1effaf349ea1aaf8aa894d2b7dcddf01968
jrsosago <jrsosago@poligran.edu.co>
2017-10-19 15:52:11 -0500

    Se finaliza la parte Front-End de Perfil

--- app/src/main/java/com/example/general/ejemplobase/MostrarEvento.java
+++ app/src/main/java/com/example/general/ejemplobase/MostrarEvento.java
@@ -12,22 +12,22 @@ import android.widget.Button;
 
 public class MostrarEvento extends AppCompatActivity {
 
-  //  Button perfilMostrarEvento;
+    Button perfilMostrarEvento;
 
-    //@Override
-    //protected void onCreate(Bundle saveInstanceState) {
-      //  super.onCreate(saveInstanceState);
-        //setContentView(R.layout.activity_mostrar_evento);
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+     setContentView(R.layout.activity_mostrar_evento);
 
-//        perfilMostrarEvento=(Button) findViewById(R.id.imageButtonPerfilMostrarEvento);
+        perfilMostrarEvento=(Button) findViewById(R.id.BotonPerfilMostrarRegistro);
 
-//        perfilMostrarEvento.setOnClickListener(new View.OnClickListener() {
-  //          @Override
-    //        public void onClick(View view) {
-      //          Intent i=new Intent(MostrarEvento.this, perfil.class);
-        //        startActivity(i);
-          //      finish();
-            //}
-        //});
-    //}
+        perfilMostrarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MostrarEvento.this, perfil.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
 }
