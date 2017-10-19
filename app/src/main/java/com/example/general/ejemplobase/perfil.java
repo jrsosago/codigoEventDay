5c78d11363773b43612d1363a77b4b3ea3e48c28
jrsosago <jrsosago@poligran.edu.co>
2017-10-19 10:50:50 -0500

    Se finaliza la parte Front-End de Mostrar un Evento.

---
+++ app/src/main/java/com/example/general/ejemplobase/perfil.java
@@ -0,0 +1,10 @@
+package com.example.general.ejemplobase;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sosa on 19/10/2017.
+ */
+
+public class perfil extends AppCompatActivity {
+}
